2cc2f9ccc7536d23d0b517400cd14aeb0e18e486
kunzoltan7707@gmail.com <kunzoltan7707@gmail.com>
2022-01-21 16:30:49 +0100

    design, metódusok rendezve

--- assets/js/table_1_order.js
+++ assets/js/table_1_order.js
@@ -3,16 +3,15 @@ var cartItems=[];
 var cart_n=document.getElementById('cart_n');
 var table=document.getElementById('table');
 var total=0;
-
+  
 //kosár tartalma
 function tableHTML(i){
 
     return `
             <tr>
                 <td style="text-align:center">${products[i].name}</td>
-                <td id="mennyiseg" style="text-align:center">1</td>                
+                <td id="mennyiseg" style="text-align:center">1</td>      
                 <td style="text-align:center">${products[i].price} Ft</td>
-                <td id="asztal1" style="text-align:center">1.asztal</td>
                 <td style="text-align:center"><input id="bem" type="button" value="Rendelés törlése" onclick="deleteRow(this)"</td>
                 </td>                                     
             </tr>
@@ -64,7 +63,7 @@ localStorage.setItem('cart', JSON.stringify(questions)); */
 //kosár kiürítése
 function clean(){
 
-    localStorage.clear();
+    localStorage.removeItem('cart')
     for(let index=0;index < products.length;index++){
 
         table.innerHTML+=tableHTML(index);
@@ -83,8 +82,11 @@ function clean(){
 
     `;
     cart_n.innerHTML='';
+    alert('A kosár törölve!')
     document.getElementById("btnBuy").style.display="none";
     document.getElementById("btnClean").style.display="none";
+
+    
 
 }
 //kosár kiürítése vége
@@ -116,54 +118,27 @@ function clean(){
                 <td></td>
                 <td></td>
                 <td></td>
+                <td class="text-warning" style="text-align:center; font-weight:bold;font-size:30px">Végösszeg: ${total} Ft</td>
                 <td></td>
-                <td class="text-warning" style="text-align:center; font-weight:bold">Végösszeg: ${total} Ft</td>
+                
              </tr>
 
              <tr class="border border-white">
              <td></td>
              <td></td>
-             <td></td>
-             <td></td>
+             <td>
+             </td>
              <td style="text-align:center"><form id="form1" action="/table_1_order" method="POST" autocomplete="off">
 
              <input type="hidden" name="total" value="${total}">
              <input type="hidden" name="_id" value="">
              <button id="submitbtn" class="btn btn-success col-md-6">Fizetés</button>
-             </form></td>
+             </form>
+             
+             </td>
+             <td></td>
+
           </tr>
-
-
-            <tr class="border border-white">
-                <td></td>
-                <td></td>
-                <td style="text-align:center">
-                <button id="btnClean" onclick="clean()" class="btn text-white btn-warning style="text-align:right">
-                Kosár kiürítése
-            </button>
-            </td>
-                <td></td>
-            <td></td>
-        </tr>
-        <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-            </tr>
-        <tr>
-        <td></td>
-        <td></td>
-        <td>   
-        <div class="d-flex justify-content-center" style="margin-top:10px;">
-        <button type="reset" class="btn btn-secondary col-md-6" style="margin-bottom: 40px; margin-left: 25px;" onclick="location.href='/table_1'">
-          Vissza a rendelésekhez
-      </button>
-       </div></td>
-        <td></td>
-            </tr>
-
-               
 
     `;
 
@@ -171,6 +146,8 @@ function clean(){
  // cart_n.innerHTML=`[${products.length}]`;
 
 })();
+
+
 
 
 var form=document.getElementById('form1');
