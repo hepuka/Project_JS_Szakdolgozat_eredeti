b136e6d3feee1c3287f8014042e425e870952a1d
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2022-09-23 16:08:21 +0200

    updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("./config/passport")(passport);
 const path = require("path");
 require("dotenv").config({ path: "config.env" });
 const mongoose = require("mongoose");
-const port = process.env.PORT;
+const port = 3000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
